032dfc7442b1ef4b4c87ef2f59ea8ac38f4a33e9
JadnarRoverdin <jadnarroverdin@gmail.com>
2017-07-17 10:30:10 -0400

    debugging

--- views/class/listCourses.php
+++ views/class/listCourses.php
@@ -3,6 +3,7 @@
 foreach($courses as $course)
 {
   echo "<strong>".$course->title."</strong><br>";
+  echo sizeof($course->classes);
   foreach($course->classes as $class)
   {
     echo "      ".$class->title."<br>";
