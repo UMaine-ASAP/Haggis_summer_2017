9a189b9604af8961bae7f8a3df6ba50770560a53
JadnarRoverdin <jadnarroverdin@gmail.com>
2017-07-18 10:51:50 -0400

    drag and drop system with data extractor

--- views/group/createGroup.php
+++ views/group/createGroup.php
@@ -21,18 +21,46 @@ function drop(ev)
   var data = ev.dataTransfer.getData("text");
   ev.target.append(document.getElementById(data));
 }
+
+function extractor()
+{
+  var groups = document.getElementsByClassName("div");
+  var part ="";
+  var i;
+  var j;
+  for(j=0; j<groups.length;j++)
+  {
+    data = groups[j].children;
+    var value="";
+    for(i=0; i<data.length;i++)
+    {
+      value = value+"<input id = 'userID[]' type='text' value='"+data[i].id+"'><br>";
+    }
+    var part = part + "<div style='border:1px solid blue'>"+value+"</div>";
+  }
+  document.getElementById("test").innerHTML = part;
+}
 </script>
-
-
-<div height="25" id = "select1" ondrop="drop(event)" ondragover="allowDrop(event)" style="border:2px solid black">
+<br>
 Group 1
-</div>
+<div class = 'div' id = "group" ondrop="drop(event)" ondragover="allowDrop(event)" style="border:2px solid black">
 
-<div id = "select2" ondrop="drop(event)" ondragover="allowDrop(event)" style="border:2px solid black">
+</div>
+<br>
 Group 2
+<div class ='div' id = "group" ondrop="drop(event)" ondragover="allowDrop(event)" style="border:2px solid black">
+
 </div>
 
 <?php
   foreach($userList as $user)
     echo "<p style='border:1px solid red' id = '".$user->id."' draggable='true' ondragstart='drag(event)'>".$user->firstName." ".$user->lastName."</p>";
 ?>
+
+<div class='test1' id="test">
+  test
+</div>
+
+<input id = 'feild' type='text' value=''>
+
+<button type="button" onclick="extractor()">TEST CLICKER</button>
