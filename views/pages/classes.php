6e18f61a4c3580c88ba77116180722833ff15b86
JadnarRoverdin <jadnarroverdin@gmail.com>
2017-08-07 09:32:47 -0400

    e

--- views/pages/classes.php
+++ views/pages/classes.php
@@ -1,5 +1,14 @@
 <h1>Classes</h1>
 
+<?php
+  foreach($classes as $class)
+  {
+    echo "<div class='classes'>";
+    echo $class->title;
+    echo "</div>";
+  }
+  ?>
+
 <a href='?controller=class&action=joinClass'>
   <div>
     Join a Class
