dfd674b6370df2a1fb4f14eb30ad33db8ac59331
JadnarRoverdin <jadnarroverdin@gmail.com>
2017-08-07 09:55:27 -0400

    e

--- controllers/pages_controller.php
+++ controllers/pages_controller.php
@@ -12,7 +12,7 @@ class PagesController
     {
       $classes = Klass::userClasses($_SESSION['token'])[1];
       echo sizeof($classes);
-      echo $classes->title;
+      echo $classes;
       require_once('views/pages/classes.php');
     }
 //=================================================================================== ASSIGNMENTS
