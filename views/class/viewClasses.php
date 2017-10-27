a551e2e783e6e3a491587aa00531e410b36ab8ac
JadnarRoverdin <jadnarroverdin@gmail.com>
2017-10-27 09:24:58 -0400

    e

--- views/class/viewClasses.php
+++ views/class/viewClasses.php
@@ -63,12 +63,12 @@ foreach($assignments as $a)
             <h2 class='targetproject' id='".$a->id."'>Choose a project to evaluate</h2>";
             foreach($a->criterias as $c)
             {
-              echo "<form><div><h3>".$c->title."</h3> on scale of ".$c->minRange." to ".$c->maxRange."<br>".$c->description."<br>";
+              echo "<div><h3>".$c->title."</h3> on scale of ".$c->minRange." to ".$c->maxRange."<br>".$c->description."<br>";
               echo "<input class='standard' name='criteriaID[]' type='hidden' value='".$c->id."'>";
               echo "<input class='standard'name ='x' id='crout'></output><br>";
               echo "<input class='standard'name='criteriaRateing[]' id='criteriaRating' type='range' max='".$c->maxRange."' min='".$c->minRange."' oninput='crout.value = criteriaRating.value'>";
               echo "<input class='standard'name='criteriaComment[]' type='text' placeholder='Comment:'>";
-              echo "</div></form><hr>";
+              echo "</div><hr>";
             }
             echo "<input class='standard' type=submit value='Submit Evaluation'>
       </form></div></td></tr></table></div>";
