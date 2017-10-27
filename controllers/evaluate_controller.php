a8b66aee5ecfdbaf4068bb146df30f7bdd244c29
JadnarRoverdin <jadnarroverdin@gmail.com>
2017-10-27 09:02:33 -0400

    e

--- controllers/evaluate_controller.php
+++ controllers/evaluate_controller.php
@@ -8,10 +8,9 @@ class EvaluateController
       $projectID = $_POST['evalfor'];
       for($i = 0; $i<sizeof($_POST['criteriaID']);$i++)
       {
-        Evaluate::submit($_POST['criteriaID'][$i], $_POST['criteriaRateing'][$i], $_POST['criteriaComment'][$i], $projectID);
+        echo Evaluate::submit($_POST['criteriaID'][$i], $_POST['criteriaRateing'][$i], $_POST['criteriaComment'][$i], $projectID)[1]."<br>";
       }
-      header('Location: index.php');
+      //header('Location: index.php');
     }
 }
-
 ?>
