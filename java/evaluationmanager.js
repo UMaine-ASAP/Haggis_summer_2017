1c1aac0bcdfb873fba0e98d58ebadc266ce43e6d
JadnarRoverdin <jadnarroverdin@gmail.com>
2017-11-03 09:09:08 -0400

    bug fixing

--- java/evaluationmanager.js
+++ java/evaluationmanager.js
@@ -6,45 +6,45 @@ $(document).ready(function()
     var currval = $(this).val();
     $('#'+curr+".criteriaRatingout").attr('value', currval);
   });
+
+  $('#evalform').submit(function(e))
+  {
+    e.preventDefault();
+    var completed = false;
+    var formsr = $('#evalform').serialize();
+    var criteriaIDs= document.getElementsByClassName('criteriaID');
+    criteriaIDs = jQuery.makeArray(criteriaIDs);
+    var criteriaRatings= document.getElementsByClassName('criteriaRating');
+    criteriaRatings = jQuery.makeArray(criteriaRatings);
+    var criteriaComment= document.getElementsByClassName('criteriaComment');
+    criteriaComment = jQuery.makeArray(criteriaComment);
+    var projectID = $('input[name="evalfor"]').val();
+    var xmlhttp = new XMLHttpRequest();
+    //alert( + " " + criteriaRatings[0].value + " " + criteriaComment[0].value + " " + projectID);
+    for(var i=0; i < criteriaIDs.length;i++)
+    {
+      var dataString = 'criteriaID='+criteriaIDs[i].value+'&criteriaRating='+criteriaRatings[i].value+'&criteriaComment='+criteriaComment[i].value+'&projectID='+projectID;
+      //alert(dataString);
+      xmlhttp.open("POST", "controllers/evaluateSlinger.php", true);
+      xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      xmlhttp.send(dataString);
+      $.ajax({
+        type: 'POST',
+        url:  '/Haggis_summer_2017/controllers/evaluateSlinger.php',
+        data: dataString,
+        success: function()
+        {
+          completed = true;
+        }
+      });
+    }
+
+    if(completed)
+    {
+      $('#evalout').append('<h3>Thank you, your feedback has been submitted<h3>');
+      $('#evalform').hide();
+    }
+    else
+      $('#evalout').append('<h3>Submission did not complete successfully<h3>');
+  }
 });
-
-function SubmitEvaluation()
-{
-  var completed = false;
-  var formsr = $('#evalform').serialize();
-  var criteriaIDs= document.getElementsByClassName('criteriaID');
-  criteriaIDs = jQuery.makeArray(criteriaIDs);
-  var criteriaRatings= document.getElementsByClassName('criteriaRating');
-  criteriaRatings = jQuery.makeArray(criteriaRatings);
-  var criteriaComment= document.getElementsByClassName('criteriaComment');
-  criteriaComment = jQuery.makeArray(criteriaComment);
-  var projectID = $('input[name="evalfor"]').val();
-  var xmlhttp = new XMLHttpRequest();
-  //alert( + " " + criteriaRatings[0].value + " " + criteriaComment[0].value + " " + projectID);
-  for(var i=0; i < criteriaIDs.length;i++)
-  {
-    var dataString = 'criteriaID='+criteriaIDs[i].value+'&criteriaRating='+criteriaRatings[i].value+'&criteriaComment='+criteriaComment[i].value+'&projectID='+projectID;
-    //alert(dataString);
-    xmlhttp.open("POST", "controllers/evaluateSlinger.php", true);
-    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xmlhttp.send(dataString);
-    $.ajax({
-      type: 'POST',
-      url:  '/Haggis_summer_2017/controllers/evaluateSlinger.php',
-      data: dataString,
-      success: function()
-      {
-        completed = true;
-      }
-    });
-  }
-
-  if(completed)
-  {
-    $('#evalout').append('<h3>Thank you, your feedback has been submitted<h3>');
-    $('#evalform').hide();
-  }
-  else
-    $('#evalout').append('<h3>Submission did not complete successfully<h3>');
-
-}
