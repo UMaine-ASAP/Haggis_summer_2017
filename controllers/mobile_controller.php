9b6aab22c7c4c8a92e982199c78bbc43d5b77d9a
Dylan Bulmer <dylanmbulmer@gmail.com>
2017-11-03 10:03:54 -0400

    A non functional update

--- controllers/mobile_controller.php
+++ controllers/mobile_controller.php
@@ -20,6 +20,49 @@ class MobileController
 //=================================================================================== CLASSES PAGE
     public function classes()
     {
+      $message = "";
+      $class;
+      $assignments;
+      $classID;
+      $idList = array();
+      $NumofGroups = 3;
+      $userList;
+
+      //fetch criterias to be used in assignment creation
+      $criterias = Criteria::all()[1];
+      $criteriaList = "<datalist id='criterias'>";
+      $criteriaStorage="";
+      foreach($criterias as $c)
+      {
+        $criteriaList .= "<option value='".$c->title."'>";
+        $criteriaStorage .= "<input type='hidden' id='".$c->title."' value='".$c->description."'>";
+      }
+      $criteriaList .="</datalist>";
+
+      //Message pulling - pulls message out of the session variables if there is any message to collect
+      if(isset($_SESSION['message']))
+      {
+        $message = $_SESSION['message'];
+        $_SESSION['message'] = "";
+      }
+      //Post routing - checks to see if user is logged in, and directs user back to the class they were in previously
+      if(isset($_SESSION['token']))
+      {
+        if(isset($_SESSION['returnto']))
+        {
+          $classID = $_SESSION['returnto'];
+          unset($_SESSION['returnto']);
+        }
+        else
+        {
+          $classID = $_GET['classID'];
+        }
+        $assignments = Assignment::classID($classID)[1];  //pulls assignments for the relevent class
+        $class = Klass::classid($classID)[1];             //pulls class data
+        $students = User::klass($classID)[1];           //pulls data for students in class
+        $userList = User::klass($classID)[1];
+      }
+      $status = 'user';                                   //checks for admin or user status
       require_once('views/mobile/classes.php');
     }
 }
