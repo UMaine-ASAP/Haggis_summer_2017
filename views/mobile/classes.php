44741b3cd4dbdcba012baf97a944709730765a56
Dylan Bulmer <dylanmbulmer@gmail.com>
2017-11-08 16:20:50 -0500

    e

--- views/mobile/classes.php
+++ views/mobile/classes.php
@@ -1,4 +1,4 @@
-<h2 class="currentPage">Classes</h2>
+<h2 class="currentPage">Add Classes</h2>
 <div class='container'>
   To add classes, enter your class code below
   <div> <?php if(isset($message)) echo $message; ?></div><br>
@@ -13,8 +13,26 @@ foreach($classes as $class)
 
   echo "<a href='?controller=mobile&action=assignments&classID=".$class->id."'><div class='classCard'>";
   echo "<table class='cardContents'>";
-  echo "<tr><td class='coursename'>".$class->coursecode."</td></tr>";
-  echo "<tr><td>".$class->title."<br></td></tr>";
+  echo "<tr><td class='courseName'>".$class->coursecode."</td></tr>";
+  echo "<tr><td class='cardContents'>".$class->title."<br></td></tr>";
+  echo "<tr><td class='cardContents'>Location: ".$class->location."</td></tr>";
+  echo "<tr><td class='cardContents'>Meets at: ".$class->timeStart." to ".$class->timeEnd."</td></tr>";
+  echo "<tr><td class='cardContents'>On: ";
+  $daySize = sizeof($class->days);
+  if($daySize > 0 )
+  {
+    for($i = 0; $i<$daySize; $i++)
+    {
+      echo substr($class->days[$i],0,2);
+      if($i == $daySize-2)
+      {
+        echo ", and ".substr($class->days[$i+1],0,2);
+        $i = $daySize;
+      }
+      else
+        echo ", ";
+    }
+  }
   echo "</table></div></a>";
 }
 
