98d28e59afb65e20927aaa6a8e5c3dcb564f6400
Dylan Bulmer <dylanmbulmer@gmail.com>
2017-11-08 17:21:28 -0500

    e

--- views/mobile/projects.php
+++ views/mobile/projects.php
@@ -6,8 +6,8 @@ Projects
   $projects = Project::assignment($_GET['assignmentID'])[1];
   foreach($projects as $a)
   {
-    echo "<tr><th>".$a->title."</th></tr>";
-    echo "<tr><td><a href='?controller=mobile&action=responses&id=".$a->id."'>See Responses</a> ";
-    echo "<a href='?controller=mobile&action=evaluate&assignmentID=".$_GET['assignmentID']."&projectID=".$a->id."'>Evaluate</a></td></tr>";
+    echo "<tr><td class='assignmentTitle push'>".$a->title."</td></tr>";
+    echo "<tr><td class='projectsButton'><a class='projectsButton' href='?controller=mobile&action=responses&id=".$a->id."'>See Responses</a>";
+    echo "<a class='projectsButton' href='?controller=mobile&action=evaluate&assignmentID=".$_GET['assignmentID']."&projectID=".$a->id."'>Evaluate</a></td></tr>";
   }
 ?>
