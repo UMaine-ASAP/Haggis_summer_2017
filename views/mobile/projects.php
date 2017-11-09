9669f03304072fd8c12743b1e26c202401ba99cc
Dylan Bulmer <dylanmbulmer@gmail.com>
2017-11-09 17:31:35 -0500

    e

--- views/mobile/projects.php
+++ views/mobile/projects.php
@@ -7,7 +7,7 @@ Projects
   foreach($projects as $a)
   {
     echo "<tr><td class='assignmentTitle push'>".$a->title."</td></tr>";
-    echo "<tr><td class='projectsButton'><a class='projectsButton' href='?controller=mobile&action=responses&id=".$a->id."'>See Responses</a>";
+    echo "<tr><td class='projectsButton'><a class='projectsButton' href='?controller=mobile&action=responses&id=".$a->id."'>See Responses</a> ";
     echo "<a class='projectsButton' href='?controller=mobile&action=evaluate&assignmentID=".$_GET['assignmentID']."&projectID=".$a->id."'>Evaluate</a></td></tr>";
   }
 ?>
