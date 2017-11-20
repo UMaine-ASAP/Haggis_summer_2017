d836435484c8aa92919d6b1e8b91cfcae78adda8
JadnarRoverdin <jadnarroverdin@gmail.com>
2017-11-20 14:37:51 -0500

    e

--- views/project/registerProject.php
+++ views/project/registerProject.php
@@ -8,13 +8,13 @@ Please submit your registration for <?php echo $assignment->title;?>
   <input class='standard' type='hidden' name='targetid' value='<?php echo $assignment->id;?>'>
   <input class='standard' type="text" name="projectname" placeholder="Project Name"><br>
   <textarea class='standard' cols='75' rows='5' name='projectdesc' placeholder='Short Description'></textarea><br>
-  <textarea class='standard' cols='75' rows='5' name='abst' placeholder='Abstract'></textarea><br>
-  <input class='standard' type="text" name='principleInvestigator' placeholder='Principle Investigator'><br>
+  <!-- <textarea class='standard' cols='75' rows='5' name='abst' placeholder='Abstract'></textarea><br> -->
+  <!-- <input class='standard' type="text" name='principleInvestigator' placeholder='Principle Investigator'><br> -->
   <input class='standard' type='number' min='1' id='numOfMembers' name ='numOfMembers' value ='1'><br>
   <div id='membercount'>
     <input class='standard' type ='text' name=firstName[] placeholder='FirstName'><input class='standard' type='text' name='middleInitial[]' max='1' placeholder='M'><input class='standard' type='text' name='lastname[]' placeholder='Last Name'><input class='standard' type='email' name='email[]' placeholder='Email Address'>
   </div>
-  <input class='standard' type='text' name='projectCode' placeholder='Project Code'><br>
+  <!-- <input class='standard' type='text' name='projectCode' placeholder='Project Code'><br> -->
 
   <input class='standard' type="submit" value="Submit Project">
 </form>
