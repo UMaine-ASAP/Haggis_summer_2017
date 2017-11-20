6c9f2e588ef450d4a4c6435802d549dd9f5c96ed
JadnarRoverdin <jadnarroverdin@gmail.com>
2017-11-20 09:13:34 -0500

    Cleaning up

--- java/evaluationmanager.js
+++ java/evaluationmanager.js
@@ -14,13 +14,13 @@ $(document).ready(function()
     console.log(formsr);
     var projectID = $('input[name="evalfor"]').val();
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function()
-    {
-      if(this.readyState==4 && this.status==200)
-      {
-        document.getElementById("evalout").append(this.responseText)
-      }
-    };
+    // xmlhttp.onreadystatechange = function()
+    // {
+    //   if(this.readyState==4 && this.status==200)
+    //   {
+    //     document.getElementById("evalout").append(this.responseText)
+    //   }
+    // };
     xmlhttp.open("POST", "?controller=evaluate&action=submit&quick=1", true);
     xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xmlhttp.send(formsr);
