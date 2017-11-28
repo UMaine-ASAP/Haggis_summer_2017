2f49508e8d2a6ae8d2101f2582a89b347ce6b935
ecurbwilson <32400978+ecurbwilson@users.noreply.github.com>
2017-11-28 15:35:30 -0500

    Lines and Such

--- views/mobile/evaluation.php
+++ views/mobile/evaluation.php
@@ -7,7 +7,7 @@
 <table  class="marginTable">
 <form id='evalform' method='post' action='?controller=mobile&action=evaluate'>
 <input type='hidden' name = 'evalfor' class='evalfor' value="<?php echo $projectid; ?>">
-
+<hr>
 <?php
   foreach($criteria as $a)
   {
@@ -19,13 +19,14 @@
     echo "<tr><td class='adjustTop push'><input class='criteriaRatingout' name='x' id='".$a->id."' value='".((int)$a->maxRange/2)."'></tr></td>";  //number
     echo "<tr><td class='adjustTop'><div id='sliderWidth'><input class='criteriaRating slider' name='criteriaRating[]' id='".$a->id."' type='range' min='".$a->minRange."' max='".$a->maxRange."'></div></tr></td>"; //slider
     if ($a->allowTextResponse) {
-      echo "<tr><td class='adjustTop center'><textarea class='commentBox' name='criteriaComment[]' type='text' placeholder='Comments...' required></textarea></tr></td>";
+      echo "<tr><td class='adjustTop center commentBot'><textarea class='commentBox' name='criteriaComment[]' type='text' placeholder='Comments...' required></textarea></tr></td>";
     }
+    echo "<tr><td><hr><td><tr>";
     echo "</div>";
   }
+
+
 ?>
-
-
-<div class="submitButton"><tr><td class="submitButton"><br><input class="submitButton" type='Submit' value='Submit'></tr></td></div>
+<tr><td class="submitButton"><br><input class="submitButton" type='Submit' value='Submit'></tr></td>
 </form>
 <div id="evalout"></div>
