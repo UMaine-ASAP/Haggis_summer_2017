a3538296b389ef0946fea51fa73bd6a036679974
Dylan Bulmer <dylanmbulmer@gmail.com>
2017-11-28 16:40:43 -0500

    Fixed toggling of buttons... yes I touched css

--- views/mobile/projects.php
+++ views/mobile/projects.php
@@ -7,8 +7,8 @@
   foreach($projects as $a)
   {
     echo "<tr><td class='assignmentTitle push' onclick='viewTab(".$a->id.")'>".$a->title."</td></tr>";
-    echo "<tr class='".$a->id."'><td class='projectsButton'><a class='responsesButton' href='?controller=mobile&action=responses&classID=".$classID."&assignmentID=".$assignmentID."&id=".$a->id."'>Responses</a>";
-    echo "<tr class='".$a->id."'><td class='projectsButton'><a class='projectsButton' href='?controller=mobile&action=evaluate&classID=".$classID."&assignmentID=".$assignmentID."&projectID=".$a->id."'>Critique</a></td></tr>";
+    echo "<tr class='tab ".$a->id."'><td class='projectsButton'><a class='responsesButton' href='?controller=mobile&action=responses&classID=".$classID."&assignmentID=".$assignmentID."&id=".$a->id."'>Responses</a>";
+    echo "<tr class='tab ".$a->id."'><td class='projectsButton'><a class='projectsButton' href='?controller=mobile&action=evaluate&classID=".$classID."&assignmentID=".$assignmentID."&projectID=".$a->id."'>Critique</a></td></tr>";
   }
 ?>
 </table>
