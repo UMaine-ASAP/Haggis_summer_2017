6ee6b4a15b93d1f4482e8b20a9a0dcd92afb30bf
JadnarRoverdin <jadnarroverdin@gmail.com>
2017-12-08 06:56:34 -0500

    Made slider generation optional in mobile evaluation generation

--- views/mobile/evaluation.php
+++ views/mobile/evaluation.php
@@ -26,15 +26,27 @@
   foreach($criteria as $a)
   {
     // Needs URL, controller function,and page to view people's projects
-    echo "<div><tr><td class='criteriaTitle push'>".$a->title."</tr></td>";
-    echo "<tr><td class='push'><span>On a scale of ".$a->minRange." to ".$a->maxRange."</span><br></tr></td>";
-    echo "<tr><td class='push'><span>".$a->description."</span><br></tr></td>";
-    echo "<tr><td><input class='criteriaID' name='criteriaID[]' type='hidden' value='".$a->id."'></tr></td>";
-    echo "<tr><td><input name='type' type='hidden' value='".$type."'></tr></td>";
-    echo "<tr><td class='adjustTop push'><input class='criteriaRatingout' name='x' id='".$a->id."' value='".((int)$a->maxRange/2)."'></tr></td>";  //number
-    echo "<tr><td class='adjustTop'><div id='sliderWidth'><input class='criteriaRating slider' name='criteriaRating[]' id='".$a->id."' type='range' min='".$a->minRange."' max='".$a->maxRange."'></div></tr></td>"; //slider
-    if ($a->allowTextResponse) {
+    if($c->minRange === $c->maxRange)
+    {
+      echo "<div><tr><td class='criteriaTitle push'>".$a->title."</tr></td>";
+      echo "<tr><td class='push'><span>".$a->description."</span><br></tr></td>";
+      echo "<tr><td><input class='criteriaID' name='criteriaID[]' type='hidden' value='".$a->id."'></tr></td>";
+      echo "<tr><td><input name='type' type='hidden' value='".$type."'></tr></td>";
+      echo "<tr><input class='criteriaRating slider' name='criteriaRating[]' id='".$a->id."' type='hidden' value='0'></div></tr></td>"; //slider
       echo "<tr><td class='adjustTop center commentBot'><textarea class='commentBox' name='criteriaComment[]' type='text' placeholder='Comments...' required></textarea></tr></td>";
+    }
+    else
+    {
+      echo "<div><tr><td class='criteriaTitle push'>".$a->title."</tr></td>";
+      echo "<tr><td class='push'><span>On a scale of ".$a->minRange." to ".$a->maxRange."</span><br></tr></td>";
+      echo "<tr><td class='push'><span>".$a->description."</span><br></tr></td>";
+      echo "<tr><td><input class='criteriaID' name='criteriaID[]' type='hidden' value='".$a->id."'></tr></td>";
+      echo "<tr><td><input name='type' type='hidden' value='".$type."'></tr></td>";
+      echo "<tr><td class='adjustTop push'><input class='criteriaRatingout' name='x' id='".$a->id."' value='".((int)$a->maxRange/2)."'></tr></td>";  //number
+      echo "<tr><td class='adjustTop'><div id='sliderWidth'><input class='criteriaRating slider' name='criteriaRating[]' id='".$a->id."' type='range' min='".$a->minRange."' max='".$a->maxRange."'></div></tr></td>"; //slider
+      if ($a->allowTextResponse) {
+        echo "<tr><td class='adjustTop center commentBot'><textarea class='commentBox' name='criteriaComment[]' type='text' placeholder='Comments...' required></textarea></tr></td>";
+      }
     }
     echo "<tr><td><hr><td><tr>";
     echo "</div>";
