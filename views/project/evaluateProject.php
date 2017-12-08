a1d8caa213bb5ce2e7dd002743eec2067800c54a
JadnarRoverdin <jadnarroverdin@gmail.com>
2017-12-08 06:48:27 -0500

    made range input optional during evaluation generation

--- views/project/evaluateProject.php
+++ views/project/evaluateProject.php
@@ -11,12 +11,24 @@ echo "</div>
 
         foreach($criterias as $c)
         {
-          echo "<div><h3>".$c->title."</h3> on scale of ".$c->minRange." to ".$c->maxRange."<br>".$c->description."<br>";
-          echo "<input class='standard criteriaID' name='criteriaID[]' type='hidden' value='".$c->id."'>";
-          echo "<input disabled class='standard criteriaRatingout' name ='x' id='".$c->id."' value='".((int)$c->maxRange/2)."'><br>";
-          echo "<input class='criteriaRating' name='criteriaRating[]' id='".$c->id."' type='range' max='".$c->maxRange."' min='".$c->minRange."' value='".((int)$c->maxRange/2)."'>";
-          echo "<textarea rows='5' cols='75' class='standard criteriaComment' name='criteriaComment[]' type='text' placeholder='Comment:'></textarea>";
-          echo "</div><hr class='minor'>";
+          if($c->minRange === $c->maxRange)
+          {
+            echo "<div><h3>".$c->title."</h3><br>".$c->description."<br>";
+            echo "<input class='standard criteriaID' name='criteriaID[]' type='hidden' value='".$c->id."'>";
+            echo "<input class='criteriaRating' name='criteriaRating[]' id='".$c->id."' type='hidden' value='0'>";
+            echo "<textarea rows='5' cols='75' class='standard criteriaComment' name='criteriaComment[]' type='text' placeholder='Comment:'></textarea>";
+            echo "</div><hr class='minor'>";
+          }
+          else
+          {
+            echo "<div><h3>".$c->title."</h3> on scale of ".$c->minRange." to ".$c->maxRange."<br>".$c->description."<br>";
+            echo "<input class='standard criteriaID' name='criteriaID[]' type='hidden' value='".$c->id."'>";
+            echo "<input disabled class='standard criteriaRatingout' name ='x' id='".$c->id."' value='".((int)$c->maxRange/2)."'><br>";
+            echo "<input class='criteriaRating' name='criteriaRating[]' id='".$c->id."' type='range' max='".$c->maxRange."' min='".$c->minRange."' value='".((int)$c->maxRange/2)."'>";
+            if($c->allowTextResponse === '1')
+              echo "<textarea rows='5' cols='75' class='standard criteriaComment' name='criteriaComment[]' type='text' placeholder='Comment:'></textarea>";
+            echo "</div><hr class='minor'>";
+          }
         }
         echo "<input class='standard' id='evalsubmit' type='button' value='Submit Evaluation'>
   </form></div></td></tr></table></div>";
