f30b780fd4a454ca865b71062de6875d539361b8
JadnarRoverdin <jadnarroverdin@gmail.com>
2017-12-14 10:19:31 -0500

    e

--- views/mobile/projects.php
+++ views/mobile/projects.php
@@ -32,7 +32,7 @@
     {
       echo "<tr><td><hr class='dropDown'><td><tr>";
       echo "<tr><td class='assignmentTitle push' onclick='viewTab(".$a->id.")'>".$a->title."</td></tr>";
-      //echo "<tr class='tab ".$a->id."'><td class='projectsButton'><a class='responsesButton' href='?controller=mobile&action=responses&classID=".$classID."&assignmentID=".$assignmentID."&id=".$a->id."'>Responses</a>";
+      echo "<tr class='tab ".$a->id."'><td class='projectsButton'><a class='responsesButton' href='?controller=mobile&action=responses&classID=".$classID."&assignmentID=".$assignmentID."&id=".$a->id."'>Responses</a>";
       echo "<tr class='tab ".$a->id."'><td class='projectsButton'><a class='projectsButton' href='?controller=mobile&action=evaluate&eventID=".$eventID."&projectID=".$a->id."'>Critique</a></td></tr>";
     }
   }
