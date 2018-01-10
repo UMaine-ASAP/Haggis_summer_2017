d94bd812074d471aaf832456e5ae95c26fe0ae1d
JadnarRoverdin <jadnarroverdin@gmail.com>
2018-01-10 07:49:47 -0500

    e

--- views/modules/scriptloader.php
+++ views/modules/scriptloader.php
@@ -1,8 +1,8 @@
 <script src="js/popup.js"></script>
 
 <script src='js/tinymce/tinymce.min.js'></script>
-<script>tinymce.init({
+<!-- <script>tinymce.init({
                       selector: '.fancyText',
                       plugins:'code',
                       toolbar:['undo redo | styleselect | bold italic | link image | alignleft aligncenter alignright | code']
-                      });</script>
+                      });</script> -->
