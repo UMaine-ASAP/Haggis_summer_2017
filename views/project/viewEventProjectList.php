a9e403c0892ad61b132267b167c7b129271b2466
Jason Dignan <jadnarroverdin@gmail.com>
2018-04-11 13:08:33 -0400

    switching livesearchmethod in eventproject list

--- views/project/viewEventProjectList.php
+++ views/project/viewEventProjectList.php
@@ -8,7 +8,7 @@ Projects(<?php echo sizeof($projectList)+sizeof($eventprojectList);?>)
 </div>
 <br>
 <div>
-  <input class='joinedInputSmaller' onkeyup='searchNhighlight(document.getElementById("searchString").value, "projects", "orange")' type='text' id='searchString' placeholder='search'><button class='joinedButtonSmaller' type='submit'><i size='smaller' class="glyphicon glyphicon-search"></i></button>
+  <input class='joinedInputSmaller' onkeyup='searchNreveal(document.getElementById("searchString").value, "projects", "orange")' type='text' id='searchString' placeholder='search'><button class='joinedButtonSmaller' type='submit'><i size='smaller' class="glyphicon glyphicon-search"></i></button>
 </div>
 <br>
 
@@ -17,12 +17,12 @@ foreach($projectList as $p)
 {
 
   if($p->id != '')
-    echo "<div class='projects' onclick='ViewEventProject(".$p->id.",".$event->id.",1)' id=''><button class='standard' style='list-style-type: none;width:80%'>".$p->title."</button></div>";
+    echo "<div class='projects' onclick='ViewEventProject(".$p->id.",".$event->id.",1)' style='display:none' id=''><button class='standard' style='list-style-type: none;width:80%'>".$p->title."</button></div>";
 }
 foreach($eventprojectList as $p)
 {
   if($p->id != '' && $p->projectEventCode != 0)
-    echo "<div class='projects' onclick='ViewEventProject(".$p->id.",".$event->id.",2)' id=''><button class='standard' style='list-style-type: none;width:80%'><strong>".$p->projectEventCode."</strong>-".$p->title."</button></div>";
+    echo "<div class='projects' onclick='ViewEventProject(".$p->id.",".$event->id.",2)' style='display:none' id=''><button class='standard' style='list-style-type: none;width:80%'><strong>".$p->projectEventCode."</strong>-".$p->title."</button></div>";
 }
 
 //if(isset($_GET['action']) && $_GET['action'] == 'createAssignment')
